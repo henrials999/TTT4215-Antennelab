8bb80253213cd312b5e37c4508541991af1702bc
killingberg <erikkjen@gmail.com>
2026-03-20 12:51:20 +0100

    added plot for phasechange with only wire

--- Data/Adapter/1_adapter_plot.py
+++ Data/Adapter/1_adapter_plot.py
@@ -5,6 +5,7 @@ import numpy as np
 path1 = "adapter_phase_fixed.s1p"
 path2 = "2adapter_phase_fixed.s1p"
 path3 = "3adapter_phase_fixed.s1p"
+path4 = "ref_cable_only_phase.s1p"
 
 
 def plot_1_adapter():
@@ -95,7 +96,40 @@ def plot_3_adapter():
 
     plt.show()
 
+    
+def plot_wire_only():
+    ntwk = rf.Network(path4)  
+    freq = ntwk.f            
+    phase = ntwk.s_deg[:,0,0] 
+    f_target = 2.4e9
+    idx = np.argmin(np.abs(freq - f_target))
+    plt.plot(freq, phase)
 
-plot_1_adapter()
-plot_2_adapter()
-plot_3_adapter()
+    plt.plot(freq[idx], phase[idx], 'ko')  # red dot
+
+    # Add text in the format (freq, phase)
+    # Format frequency to 2 decimal GHz, phase to 1 decimal deg
+    label_text = f"({freq[idx]/1e9:.2f} GHz, {phase[idx]:.1f}°)"
+    plt.annotate(label_text,
+                xy=(freq[idx], phase[idx]),
+                xytext=(5, 0),          # offset text slightly
+                textcoords='offset points',
+                color='black',
+                fontsize=10,
+                )
+
+
+
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Phase (deg)")
+    plt.title("S11 Phase wire only")
+    plt.savefig("S11_Phase_wire_only.png", dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+
+
+#plot_1_adapter()
+#plot_2_adapter()
+#plot_3_adapter()
+plot_wire_only()
